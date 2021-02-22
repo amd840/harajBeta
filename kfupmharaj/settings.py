8f2847eae674225f0dfcaba3d47ff240d56011ee
amd840 <s201634600>
2021-02-22 23:40:31 +0300

    heroku

--- kfupmharaj/settings.py
+++ kfupmharaj/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,3 +128,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
